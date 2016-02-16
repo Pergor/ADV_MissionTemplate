2ce1f0147ed3de4ae865b6fd553ea275f214b5ac
Belbo <adrian_verscharen@yahoo.de>
2016-02-16 14:25:35 +0100

    Esseker version added

---
+++ ADV_MissionTemplate.Esseker/mission/ADV_defines.hpp
@@ -0,0 +1,6 @@
+﻿#define MISSIONAUTHOR "[SeL] Belbo"
+#define MISSIONNAME "ADV Template Esseker"
+#define MISSIONSUBTITLE "Viel Spaß beim Spielen!"
+#define MISSIONTEASER "Viel Spaß!"
+#define ONLOADINTRO "Viel Spaß!"
+#define MISSIONPLAYERS 60
