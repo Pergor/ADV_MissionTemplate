42c9a7485d73fc10a1a564261fd001f8f4131ede
Belbo <adrian_verscharen@yahoo.de>
2016-05-19 01:45:45 +0200

    moved unused map versions to separate folder

--- ADV_MissionTemplate.fata/mission/ADV_defines.hpp
+++
@@ -1,6 +0,0 @@
-﻿#define MISSIONAUTHOR "[SeL] Belbo"
-#define MISSIONNAME "ADV Template FATA"
-#define MISSIONSUBTITLE "Viel Spaß beim Spielen!"
-#define MISSIONTEASER "Viel Spaß!"
-#define ONLOADINTRO "Viel Spaß!"
-#define MISSIONPLAYERS 60
