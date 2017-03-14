1a3cac80408e47e4205e6ac4e3a5f1a4b0aa962c
Belbo <adrian_verscharen@yahoo.de>
2017-03-14 17:21:03 +0100

    moved baranow version to old versions.

--- adv_ifltemplate.baranow/functions/gearsaving/fn_saveGear.sqf
+++
@@ -1,5 +0,0 @@
-﻿params [
-	["_unit", player, [objNull]]
-];
-
-adv_saveGear_loadout = getUnitLoadout _unit;
--- adv_ifltemplate.baranow/mission/ADV_defines.hpp
+++
@@ -1,6 +0,0 @@
-﻿#define MISSIONAUTHOR "[SeL] Belbo"
-#define MISSIONNAME "ADV - IFL Template by Belbo"
-#define MISSIONSUBTITLE "Viel Spaß beim Spielen!"
-#define MISSIONTEASER "Viel Spaß!"
-#define ONLOADINTRO "Viel Spaß!"
-#define MISSIONPLAYERS 13
