baae25d143fbd66fd6bd0432417e3676aadc7c66
Belbo <adrian_verscharen@yahoo.de>
2017-05-21 14:40:01 +0200

    MOVED: Moved nimitz templates to old versions

--- adv_nimitztemplate.stratis/mission/ADV_defines.hpp
+++
@@ -1,6 +0,0 @@
-﻿#define MISSIONAUTHOR "[SeL] Belbo"
-#define MISSIONNAME "ADV - Nimitz Template by Belbo"
-#define MISSIONSUBTITLE "Viel Spaß beim Spielen!"
-#define MISSIONTEASER "Viel Spaß!"
-#define ONLOADINTRO "Viel Spaß!"
-#define MISSIONPLAYERS 49
