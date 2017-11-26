965568c03718e2dcc5e4bf1ee11fd6c21b2e7578
Belbo <adrian_verscharen@yahoo.de>
2017-11-26 19:24:48 +0100

    IMPROVED: moved a couple of things around.

--- public/ai package/adv_ai/cfgfunctions.hpp
+++ public/ai package/adv_ai/cfgfunctions.hpp
@@ -3,16 +3,20 @@ class adv_ai
 	tag = "adv_ai";
 	class standard
 	{
-		file = adv_ai";
+		file = "adv_ai";
 		class aiTask {};
+		class spawnConvoy {};
+		class spawnGroup {};
+	};
+	class internal
+	{
+		file = "adv_ai\internal";
 		class findNearestEnemy {};
 		class getGroupVehicles {};
 		class getPos {};
 		class setSafe {};
 		class setSide {};
 		class setSkill {};
-		class spawnConvoy {};
-		class spawnGroup {};
 		class ZenOccupyHouse {};
 	};
 };
